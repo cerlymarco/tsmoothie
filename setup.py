894cbc5cd825ba0659e281f515f6d18317b91a22
cerlymarco <36955807+cerlymarco@users.noreply.github.com>
2020-08-24 01:41:56 +0200

    Release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = '0.1.2'
+VERSION = '0.1.3'
 PACKAGE_NAME = 'tsmoothie'
 AUTHOR = 'Marco Cerliani'
 AUTHOR_EMAIL = 'cerlymarco@gmail.com'
